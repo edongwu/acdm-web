f904dce66a174122652909dc77a5ef1e42324c9b
yd_wu <wuyidong007@163.com>
2019-02-14 10:48:17 +0800

    v1.0
    初次发布

---
+++ files/目标设置/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t),u,_(),v,_(w,x,y,z,A,B,C,_(),D,_(E,F,G,H,I,_(J,K,L,M),N,null,O,H,P,H,Q,R,S,null,T,U,V,W,X,Y,Z,U),ba,_(),bb,_(),bc,_(bd,[])),be,_(),bf,_());}; 
+var b="url",c="目标设置.html",d="generationDate",e=new Date(1550112436989.47),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="defaultBackgroundImageInfo",o="path",p="images/全国机场运行/bg.jpg",q="width",r=1600,s="height",t=960,u="viewIdToBackgroundImageInfo",v="page",w="packageId",x="67204a390b1540b4993b46dec457a76a",y="type",z="Axure:Page",A="name",B="目标设置",C="notes",D="style",E="baseStyle",F="627587b6038d43cca051c114ac41ad32",G="pageAlignment",H="near",I="fill",J="fillType",K="solid",L="color",M=0xFFFFFFFF,N="image",O="imageHorizontalAlignment",P="imageVerticalAlignment",Q="imageRepeat",R="auto",S="favicon",T="sketchFactor",U="0",V="colorStyle",W="appliedColor",X="fontName",Y="Applied Font",Z="borderWidth",ba="adaptiveStyles",bb="interactionMap",bc="diagram",bd="objects",be="masters",bf="objectPaths";
+return _creator();
+})());
